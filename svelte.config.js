f9e959b1d7f37a2366207573f994b483a6cc3f70
Andrei Ayar Titiriga <andrei.titiriga@toluna.com>
2022-09-30 19:33:47 +0300

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+	// for more information about preprocessors  
 	preprocess: [
 		preprocess({
 			postcss: true
